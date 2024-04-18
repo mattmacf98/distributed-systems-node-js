0fc0a8a30aa62fae168118a29cc4a54b66929f52
Matthew MacFarquhar <macfarqu@amazon.com>
2024-04-18 10:46:08 -0400

    chapter 1 why distributed

---
+++ web-api/consumer-http-basic.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+const server = require('fastify')();
+const fetch = require('node-fetch');
+const HOST = process.env.HOST || '127.0.0.1';
+const PORT = process.env.PORT || 3000;
+const TARGET = process.env.TARGET || 'localhost:4000';
+
+server.get('/', async () => {
+   const res = await fetch(`http://${TARGET}/recipes/42`);
+   const producer_data = await res.json();
+
+   return {
+       consumer_pid: process.pid,
+       producer_data: producer_data
+   };
+});
+
+server.listen(PORT, HOST, () => {
+    console.log(`Consumer running at http://${HOST}:${PORT}`)
+})
